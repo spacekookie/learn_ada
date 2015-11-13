0ecb5af379cfa723c490ec39a2a95c9856b1fa49
Katharina Sabel <sabel.katharina@gmail.com>
2015-11-13 17:25:22 +0100

    initial commit.

--- e_c02_p1.adb
+++ e_c02_p1.adb
@@ -1,8 +1,18 @@
-  with Ada.Text_IO     ;         use Ada.Text_IO;procedure UglyForm
-is begin Put   ("Good form ")                   ;Put("can aid in ")
-;Put  ("understanding a program,");New_Line;Put
-("and bad form ");Put   ("can make a program ");Put("unreadable.");
-   New_Line;end UglyForm;
+with Ada.Text_IO;
+use Ada.Text_IO;
+
+procedure UglyForm is 
+begin 
+    Put("Good form ");
+    Put("can aid in ");
+    Put ("understanding a program,");
+    New_Line;
+    
+    Put("and bad form ");
+    Put("can make a program ");
+    Put("unreadable.");
+    New_Line;
+end UglyForm;
 
 
 
