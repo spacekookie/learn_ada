99dde4caeb8cffd77d6569c5223cec105057ac98
Katharina Sabel <sabel.katharina@gmail.com>
2016-04-17 11:44:02 +0200

    Refactoring some simple examples and adding a more complicated example with nested procedures.

---
+++ compute.adb
@@ -0,0 +1,21 @@
+-- Demonstrate a trivial procedure with another nested inside
+
+with Ada.Text_IO, Ada.Integer_Text_IO;
+use Ada.Text_IO, Ada.Integer_Text_IO;
+
+procedure Compute is
+    
+    procedure Double(Item : in out Integer) is
+    begin
+        Item := Item * 2;
+    end Double;
+
+    X : Integer := 1; -- Local variable X of type int
+
+begin -- Actually begin the procedure compute
+    loop
+        Put(X);
+        New_Line;
+        Double(X);
+    end loop;
+end Compute;
